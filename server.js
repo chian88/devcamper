6c1694b05222ec16c65f1b63182ab3b1261ee9c1
chian yee <chianyee.lee@carsome.com>
2022-10-05 17:26:12 +0800

    setup express

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const dotenv = require('dotenv');
+
+dotenv.config({path: './config/config.env'});
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`server running in ${PORT} in environment ${process.env.NODE_ENV}`));
